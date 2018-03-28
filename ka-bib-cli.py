b251f81c9bd7087433bc0f4988f22455b35cf2dc
Lesmana Zimmer <lesmana@gmx.de>
2018-03-28 13:21:35 +0200

    directly to login

--- ka-bib-cli.py
+++ ka-bib-cli.py
@@ -3,7 +3,12 @@
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get('https://karlsruhe.bibdia-mobil.de/')
-welcomeelement = driver.find_element_by_id('welcome')
-assert 'Stadtbibliothek Karlsruhe' in welcomeelement.text
+driver.get('https://karlsruhe.bibdia-mobil.de/?action=konto')
+usernamefield = driver.find_element_by_id('unr')
+usernamefield.clear()
+usernamefield.send_keys('username')
+passwordfield = driver.find_element_by_id('password')
+passwordfield.clear()
+passwordfield.send_keys('password')
+loginbutton = driver.find_element_by_id('loginbtn')
 driver.quit()
