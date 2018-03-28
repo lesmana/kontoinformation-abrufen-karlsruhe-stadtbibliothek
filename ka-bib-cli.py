11b04c279d4d19566592e5d851ef314ff10f1827
Lesmana Zimmer <lesmana@gmx.de>
2018-03-28 22:20:57 +0200

    main function

--- ka-bib-cli.py
+++ ka-bib-cli.py
@@ -4,17 +4,22 @@ from selenium import webdriver
 
 import secret
 
-options = webdriver.FirefoxOptions()
-options.set_headless(True)
-driver = webdriver.Firefox(options=options)
-driver.get('https://karlsruhe.bibdia-mobil.de/?action=konto')
-usernamefield = driver.find_element_by_id('unr')
-usernamefield.clear()
-usernamefield.send_keys(secret.username)
-passwordfield = driver.find_element_by_id('password')
-passwordfield.clear()
-passwordfield.send_keys(secret.password)
-loginbutton = driver.find_element_by_id('loginbtn')
-loginbutton.click()
-assert 'Leserkonto' in driver.title
-driver.quit()
+def main():
+  print('start ka bib cli')
+  options = webdriver.FirefoxOptions()
+  options.set_headless(True)
+  driver = webdriver.Firefox(options=options)
+  driver.get('https://karlsruhe.bibdia-mobil.de/?action=konto')
+  usernamefield = driver.find_element_by_id('unr')
+  usernamefield.clear()
+  usernamefield.send_keys(secret.username)
+  passwordfield = driver.find_element_by_id('password')
+  passwordfield.clear()
+  passwordfield.send_keys(secret.password)
+  loginbutton = driver.find_element_by_id('loginbtn')
+  loginbutton.click()
+  assert 'Leserkonto' in driver.title
+  driver.quit()
+
+if __name__ == '__main__':
+  main()
