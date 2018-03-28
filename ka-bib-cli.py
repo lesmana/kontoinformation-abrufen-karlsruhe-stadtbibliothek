cf3026a1363eb7f3cb1efe5c09be6f888687ed59
Lesmana Zimmer <lesmana@gmx.de>
2018-03-28 13:11:06 +0200

    use mobile website which is better organized

--- ka-bib-cli.py
+++ ka-bib-cli.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get('https://opac.karlsruhe.de/')
-assert "Stadtbibliothek" in driver.title
+driver.get('https://karlsruhe.bibdia-mobil.de/')
+welcomeelement = driver.find_element_by_id('welcome')
+assert 'Stadtbibliothek Karlsruhe' in welcomeelement.text
 driver.quit()
