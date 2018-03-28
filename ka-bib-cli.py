9f7df0b36fb1705b697e7a796c4eb197f59d22a1
Lesmana Zimmer <lesmana@gmx.de>
2018-03-28 13:26:32 +0200

    secret username and password

--- ka-bib-cli.py
+++ ka-bib-cli.py
@@ -2,13 +2,17 @@
 
 from selenium import webdriver
 
+import secret
+
 driver = webdriver.Firefox()
 driver.get('https://karlsruhe.bibdia-mobil.de/?action=konto')
 usernamefield = driver.find_element_by_id('unr')
 usernamefield.clear()
-usernamefield.send_keys('username')
+usernamefield.send_keys(secret.username)
 passwordfield = driver.find_element_by_id('password')
 passwordfield.clear()
-passwordfield.send_keys('password')
+passwordfield.send_keys(secret.password)
 loginbutton = driver.find_element_by_id('loginbtn')
+loginbutton.click()
+assert 'Leserkonto' in driver.title
 driver.quit()
