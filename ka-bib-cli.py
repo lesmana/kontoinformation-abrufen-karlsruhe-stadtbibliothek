4b411a648edcf66eed549ee5cf04477cf19b6958
Lesmana Zimmer <lesmana@gmx.de>
2021-06-22 23:19:55 +0200

    print info

--- ka-bib-cli.py
+++ ka-bib-cli.py
@@ -5,6 +5,7 @@ import requests
 import bs4
 import pprint
 import time
+import datetime
 
 baseurl = 'https://opac.karlsruhe.de'
 
@@ -41,6 +42,39 @@ assert(response.status_code == 200)
 
 soup = bs4.BeautifulSoup(response.content, 'html.parser')
 
-tableelems = soup.find_all('table', attrs={'class': 'tab21'})
-for tableelem in tableelems:
-  print(tableelem.prettify())
+tables = soup.find_all('table', attrs={'class': 'tab21'})
+
+infotable = tables[0]
+
+if len(tables) == 1:
+  borrowtable = None
+elif len(tables) == 2:
+  borrowtable = tables[1]
+else:
+  print('unexpected number of tables')
+  print('maybe layout of website changed')
+  for table in tables:
+    print(table.prettify())
+  sys.exit(1)
+
+infotds = infotable.find_all('td')
+
+name = ''.join(infotds[1].stripped_strings)
+print('name', name)
+
+fee = ''.join(infotds[2].stripped_strings)
+if fee != '':
+  print('gebühren', fee)
+
+validity = ''.join(infotds[5].stripped_strings)
+_, validity = validity.split()
+delta = datetime.datetime.strptime(validity, '%d.%m.%Y') - datetime.datetime.today()
+#print(validity)
+#print(delta.days)
+
+if delta.days < 0:
+  print(f'ausweis abgelaufen ({validity})')
+  print(f'in tagen: {delta.days}')
+elif delta.days < 14:
+  print(f'ausweis läuft bald ab ({validity})')
+  print(f'in tagen: {delta.days}')
